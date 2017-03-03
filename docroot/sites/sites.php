55786b538cdab0458548265bbab3b350644a951c
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-03-03 13:49:41 -0700

    enabled multisite for local installs.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,8 +2,9 @@
 
     $sites = array(
       'coloradod8mdev.prod.acquia-sites.com.site1' => 'site1',
-      'coloradod8mdev.prod.acquia-sites.com.site1' => 'site1',
       'coloradod8mdev.prod.acquia-sites.com.site1.com' => 'site1',
       'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
+      'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
+      '8888.coloradod8m.site1' => 'site1',
     );
 ?>
