af4d7b0718f94cec6bd6934d452d246847767341
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-03-09 10:10:15 -0700

    adding subdomain path.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -6,5 +6,9 @@
       'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
       'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
       '8888.coloradod8m.site1' => 'site1',
+
     );
+
+    $sites['coloradod8mdev.prod.acquia-sites.com'] = 'default';
+    $sites['site1.coloradod8mdev.com'] = 'site1';
 ?>
