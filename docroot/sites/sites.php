e0476977c2a4e5937a7bb41f39100351450e6bad
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 10:47:35 -0600

    cleanup of sites

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,7 +1,5 @@
 <?php
 
 $sites = [
-    // http://dogs.kalose.net/
-    'pilot-dev.ci.colorado.gov' => 'coloradointeractive',
     'pilot-dev.colorado.gov.coloradointeractive' => 'coloradointeractive',
 ];
