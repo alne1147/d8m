0f5fac0ce986562d8e05a08964bd3bb035f1c04f
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 10:14:20 -0600

    adding sub directory.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -3,4 +3,5 @@
 $sites = array(
     // http://dogs.kalose.net/
     'ci.pilot-dev.colorado.gov' => 'coloradointeractive',
+    'pilot-dev.colorado.gov.coloradointeractive' => 'coloradointeractive',
 );
