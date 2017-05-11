f93af8e3becae247fa65b81fb6787f610f6f78c1
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 10:23:11 -0600

    new line.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -5,3 +5,5 @@ $sites = array(
     'ci.pilot-dev.colorado.gov' => 'coloradointeractive',
     'pilot-dev.colorado.gov.coloradointeractive' => 'coloradointeractive',
 );
+
+$sites['ci.pilot-dev.colorado.gov'] = 'coloradointeractive';
