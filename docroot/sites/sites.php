c52cd61d2c65b666dd035b6bbd0cb49b71ece4df
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 09:49:32 -0600

    domain fix.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,4 +2,4 @@
 
 
 $sites[' pilot-ci.colorado.gov'] = 'pilot-ci.colorado.gov';
-$sites[' ci.pilot.colorado.gov'] = 'coloradointeractive';
+$sites[' ci.pilot-dev.colorado.gov'] = 'coloradointeractive';
