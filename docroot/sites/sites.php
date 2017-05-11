ac9cd3e62ec541d9d618dbb5ed592a3536a1b575
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 13:33:33 -0600

    adding cdle sub domain attempt.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,6 +2,8 @@
 
 $sites = [
     'pilot-dev.colorado.gov.coloradointeractive' => 'coloradointeractive',
-    'pilot-dev.colorado.gov.cdle' => 'cdle',
+    'pilot-cdle.colorado.gov.coloradointeractive' => 'cdle',
+    'cdle.pilot-dev.colorado.gov.coloradointeractive' => 'cdle',
 
 ];
+
