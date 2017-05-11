b3368f789d411b390795bb9689fdda4c301bac2a
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-11 09:58:08 -0600

    adding new domain.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,6 +1,4 @@
 <?php
 
-
-$sites['pilot-ci.colorado.gov'] = 'pilot-ci.colorado.gov';
 $sites['ci.pilot-dev.colorado.gov'] = 'coloradointeractive';
-$sites['ci.pilot-dev.colorado.gov'] = 'coloradointeractive';
+$sites['pilot-dev.colorado.gov.ci'] = 'coloradointeractive';
