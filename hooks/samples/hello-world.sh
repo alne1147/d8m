6be3e593f94644ffbd106969a9a39511980bc52e
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-15 10:56:15 -0600

    correcting sample file.

--- hooks/samples/hello-world.sh
+++ hooks/samples/hello-world.sh
@@ -3,9 +3,5 @@
 # This sample Cloud Hook script just echos "Hello, Cloud!" to standard
 # output. It will work in any hook directory.
 
-site=$1
-target_env=$2
-drush_alias=$site'.'$target_env
+echo "Hello, Cloud!"
 
-# Execute a standard drush command.
-drush @$drush_alias st
