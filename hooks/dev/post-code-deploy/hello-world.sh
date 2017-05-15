d20472c05596cb1b0a77e898ae0ef509f7edf80e
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-15 10:56:15 -0600

    correcting sample file.

--- hooks/dev/post-code-deploy/hello-world.sh
+++ hooks/dev/post-code-deploy/hello-world.sh
@@ -2,5 +2,15 @@
 #
 # This sample Cloud Hook script just echos "Hello, Cloud!" to standard
 # output. It will work in any hook directory.
+#
+# This sample Cloud Hook script just echos "Hello, Cloud!" to standard
+# output. It will work in any hook directory.
+
+site=$1
+target_env=$2
+drush_alias=$site'.'$target_env
+
+# Execute a standard drush command.
+drush @$drush_alias st
 
 echo "Hello, Cloud!"
