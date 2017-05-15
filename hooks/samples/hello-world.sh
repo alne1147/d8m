1f748a1a12d220544c914c3afac756529d27c3dd
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-15 10:52:00 -0600

    testing st again.

--- hooks/samples/hello-world.sh
+++ hooks/samples/hello-world.sh
@@ -3,17 +3,6 @@
 # This sample Cloud Hook script just echos "Hello, Cloud!" to standard
 # output. It will work in any hook directory.
 
-echo "Hello, Cloud!"
-
-
-#
-# Cloud Hook: drush-cache-clear
-#
-# Run drush cache-clear all in the target environment. This script works as
-# any Cloud hook.
-
-
-# Map the script inputs to convenient names.
 site=$1
 target_env=$2
 drush_alias=$site'.'$target_env
