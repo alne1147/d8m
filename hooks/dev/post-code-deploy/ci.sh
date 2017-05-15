cb117dd79268a456d89a8a9bf9dfcf436886538b
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-05-15 11:51:08 -0600

    testing multisite cache clear and crawling to site directory.

--- hooks/dev/post-code-deploy/ci.sh
+++ hooks/dev/post-code-deploy/ci.sh
@@ -6,14 +6,8 @@
 # This sample Cloud Hook script just echos "Hello, Cloud!" to standard
 # output. It will work in any hook directory.
 
-site=$1
-target_env=$2
-drush_alias=$site'.'$target_env
-
-# Execute a standard drush command.
-drush @$site.$target_env -l www.pilot-stg.colorado.gov updatedb --yes
-
 
 echo "Hello, Cloud!"
 
-ls
+cd /var/www/html/coloradod8mdev/docroot
+drush @sites cr
