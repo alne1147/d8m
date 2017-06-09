ea209eef8134824e383f99de4285efaa26fc639e
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-06-09 10:30:58 -0600

    sites all no longer ignored so multisites can use same service.yml. Added domains to sites.php file. The first 10 domains for dev and stage are in this version.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,9 +1,58 @@
 <?php
 
 $sites = [
-    'pilot-dev.colorado.gov.coloradointeractive' => 'coloradointeractive',
-    'pilot-stg.colorado.gov' => 'cdle',
-    'cdle.pilot-dev.colorado.gov.coloradointeractive' => 'cdle',
+    
+    // Dept. of Agriculture
+    'ag.dev.colorado.gov' => 'ag.colorado.gov',
+    'ag.stg.colorado.gov' => 'ag.colorado.gov',
+    
+    // Dept. of Public Safety
+    'cdps.dev.colorado.gov' => 'cdps',
+    'cdps.stg.colorado.gov' => 'cdps',
+    
+    // Dept. of Personnel and Administration
+    'dpa.dev.colorado.gov' => 'dpa',
+    'dpa.stg.colorado.gov' => 'dpa',
+    
+    // Dept. of Revenue
+    'revenue.dev.colorado.gov' => 'revenue',
+    'revenue.stg.colorado.gov' => 'revenue',
+    
+    // Dept. of Regulatory Agencies
+    'dora.dev.colorado.gov' => 'dora',
+    'dora.stg.colorado.gov' => 'dora',
+    
+    // Dept. of Local Affairs
+    'dola.dev.colorado.gov' => 'dola',
+    'dola.stg.colorado.gov' => 'dola',
+    
+    // Dept. of Healthcare Policy and Financing
+    'hcpf.dev.colorado.gov' => 'hcpf',
+    'hcpf.stg.colorado.gov' => 'hcpf',
+    
+    // Dept. of Labor
+    'cdle.dev.colorado.gov' => 'cdle',
+    'cdle.stg.colorado.gov' => 'cdle',
+    
+    // Dept. of Public Health and Environment
+    'cdphe.dev.colorado.gov' => 'cdphe',
+    'cdphe.stg.colorado.gov' => 'cdphe',
+    
+    // Town of Estes Park
+    'estes.dev.colorado.gov' => 'estes',
+    'estes.stg.colorado.gov' => 'estes',
+    
+    // State Internet Portal Authority
+    'sipa.dev.colorado.gov' => 'sipa',
+    'sipa.dev.colorado.gov' => 'sipa',
+    
+    // State Land Board
+    'slb.dev.colorado.gov' => 'slb',
+    'slb.stg.colorado.gov' => 'slb',
+    
+    // Colorado Interactive
+    'ci.dev.colorado.gov' => 'ci',
+    'ci.stg.colorado.gov' => 'ci',
 
 ];
 
