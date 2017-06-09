c8dac7147e74a2a184160c6108b62281930ca48f
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-06-09 13:12:01 -0600

    corrected directory names of sites.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -7,52 +7,52 @@ $sites = [
     'ag.stg.colorado.gov' => 'ag.colorado.gov',
     
     // Dept. of Public Safety
-    'cdps.dev.colorado.gov' => 'cdps',
-    'cdps.stg.colorado.gov' => 'cdps',
+    'cdps.dev.colorado.gov' => 'cdps.colorado.gov',
+    'cdps.stg.colorado.gov' => 'cdps.colorado.gov',
     
     // Dept. of Personnel and Administration
-    'dpa.dev.colorado.gov' => 'dpa',
-    'dpa.stg.colorado.gov' => 'dpa',
+    'dpa.dev.colorado.gov' => 'dpa.colorado.gov',
+    'dpa.stg.colorado.gov' => 'dpa.colorado.gov',
     
     // Dept. of Revenue
-    'revenue.dev.colorado.gov' => 'revenue',
-    'revenue.stg.colorado.gov' => 'revenue',
+    'revenue.dev.colorado.gov' => 'revenue.colorado.gov',
+    'revenue.stg.colorado.gov' => 'revenue.colorado.gov',
     
     // Dept. of Regulatory Agencies
-    'dora.dev.colorado.gov' => 'dora',
-    'dora.stg.colorado.gov' => 'dora',
+    'dora.dev.colorado.gov' => 'dora.colorado.gov',
+    'dora.stg.colorado.gov' => 'dora.colorado.gov',
     
     // Dept. of Local Affairs
-    'dola.dev.colorado.gov' => 'dola',
-    'dola.stg.colorado.gov' => 'dola',
+    'dola.dev.colorado.gov' => 'dola.colorado.gov',
+    'dola.stg.colorado.gov' => 'dola.colorado.gov',
     
     // Dept. of Healthcare Policy and Financing
-    'hcpf.dev.colorado.gov' => 'hcpf',
-    'hcpf.stg.colorado.gov' => 'hcpf',
+    'hcpf.dev.colorado.gov' => 'hcpf.colorado.gov',
+    'hcpf.stg.colorado.gov' => 'hcpf.colorado.gov',
     
     // Dept. of Labor
-    'cdle.dev.colorado.gov' => 'cdle',
-    'cdle.stg.colorado.gov' => 'cdle',
+    'cdle.dev.colorado.gov' => 'cdle.colorado.gov',
+    'cdle.stg.colorado.gov' => 'cdle.colorado.gov',
     
     // Dept. of Public Health and Environment
-    'cdphe.dev.colorado.gov' => 'cdphe',
-    'cdphe.stg.colorado.gov' => 'cdphe',
+    'cdphe.dev.colorado.gov' => 'cdphe.colorado.gov',
+    'cdphe.stg.colorado.gov' => 'cdphe.colorado.gov',
     
     // Town of Estes Park
-    'estes.dev.colorado.gov' => 'estes',
-    'estes.stg.colorado.gov' => 'estes',
+    'estes.dev.colorado.gov' => 'estes.colorado.gov',
+    'estes.stg.colorado.gov' => 'estes.colorado.gov',
     
     // State Internet Portal Authority
-    'sipa.dev.colorado.gov' => 'sipa',
-    'sipa.dev.colorado.gov' => 'sipa',
+    'sipa.dev.colorado.gov' => 'sipa.colorado.gov',
+    'sipa.dev.colorado.gov' => 'sipa.colorado.gov',
     
     // State Land Board
-    'slb.dev.colorado.gov' => 'slb',
-    'slb.stg.colorado.gov' => 'slb',
+    'slb.dev.colorado.gov' => 'slb.colorado.gov',
+    'slb.stg.colorado.gov' => 'slb.colorado.gov',
     
     // Colorado Interactive
-    'ci.dev.colorado.gov' => 'ci',
-    'ci.stg.colorado.gov' => 'ci',
+    'ci.dev.colorado.gov' => 'ci.colorado.gov',
+    'ci.stg.colorado.gov' => 'ci.colorado.gov',
 
 ];
 
