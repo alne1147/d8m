1ae3348ef818680aea7cfbe88f04775f78d9a7f6
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-06-27 10:35:00 -0600

    removing unused settings file. Turning debug off.

--- docroot/sites/all/settings.d8m.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$config_directories['sync'] = '../config/sync';
-$settings['install_profile'] = 'ci_start';
-$config['content_directory'] = '../content';
-$conf['file_temporary_path'] = 'tmp';
