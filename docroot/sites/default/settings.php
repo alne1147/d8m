a01b5d98ca88715bb1ad92903d051686625ef04b
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-07-31 10:48:02 -0600

    removed site name from default. In hopes that sub sites will read their own.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -775,4 +775,3 @@ $config_directories['sync'] = '../config/sync';
 $settings['install_profile'] = 'ci_start';
 $config['content_directory'] = '../content';
 $conf['file_temporary_path'] = 'tmp';
-$config['system.site']['name'] = 'CI D8M';
