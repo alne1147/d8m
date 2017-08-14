dba2108ab522af5e226db6d84d7a96d4de3e2d88
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-08-14 15:56:07 -0600

    set a general type for selection in advanced theme.

--- docroot/themes/ci_xy/theme-settings.php
+++ docroot/themes/ci_xy/theme-settings.php
@@ -1,15 +1,31 @@
 <?php
 
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Theme\ThemeSettings;
+use Drupal\system\Form\ThemeSettingsForm;
+use Drupal\Core\Form;
+
 function ci_xy_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface &$form_state, $form_id = NULL) {
     // Work-around for a core bug affecting admin themes. See issue #943212.
     if (isset($form_id)) {
         return;
     }
 
-    $form['foo_example'] = array(
-        '#type'          => 'textfield',
-        '#title'         => t('Widget'),
-        '#default_value' => theme_get_setting('foo_example'),
-        '#description'   => t("Place this text in the widget spot on your site."),
+
+    # the values for the dropdown box
+    $form['type_options'] = array(
+        '#type' => 'value',
+        '#value' => array('APPLICATION' => t('Application'),
+            'DEVELOPMENT' => t('Development'),
+            'ENHANCEMENT' => t('Enhancement'))
     );
+    $form['type'] = array(
+        '#title' => t('Project Type'),
+        '#type' => 'select',
+        '#description' => "Select the project count type.",
+        '#default_value' => theme_get_setting('type'),
+        '#options' => $form['type_options']['#value'],
+    );
+
 }
