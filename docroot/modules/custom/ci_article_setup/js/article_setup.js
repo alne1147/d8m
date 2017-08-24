6a437ff4be1e5655fe2dc3b4d3d7b4f6c7505b22
alne1147 <alne1147@live.com>
2017-08-23 19:14:38 -0600

    added simple alert.

--- docroot/modules/custom/ci_article_setup/js/article_setup.js
+++ docroot/modules/custom/ci_article_setup/js/article_setup.js
@@ -1,3 +1,7 @@
-$( document ).ready(function() {
-    alert("Your book is overdue.");
-}
+Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
+        // Using once() to apply the myCustomBehaviour effect when you want to do just run one function.
+        alert("Your book is overdue.");
+        // Using once() with more complexity.
+    }
+};
