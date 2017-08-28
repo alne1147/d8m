bda022f832ab6ec99b2f7a064174a52aebca828d
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-08-28 14:04:55 -0600

    add show and hide to author_date and author_name.

--- docroot/modules/custom/ci_article_setup/js/article_setup.js
+++ docroot/modules/custom/ci_article_setup/js/article_setup.js
@@ -4,7 +4,7 @@
     Drupal.behaviors.authorDisplay = {
         attach: function(context, settings) {
             $('div.display-options:contains("Show Author")').each(function () {
-                $('.author_name').addClass("show"); // matched td add NewClass
+                $('.author_name').removeClass( "hide" ).addClass( "show" );
             });
         }
     };
@@ -12,7 +12,7 @@
     Drupal.behaviors.dateDisplay = {
         attach: function(context, settings) {
             $('div.display-options:contains("Show Post Date")').each(function () {
-                $('.author_date').addClass("show"); // matched td add NewClass
+                $('.author_date').removeClass( "hide" ).addClass( "show" );
             });
         }
     };
