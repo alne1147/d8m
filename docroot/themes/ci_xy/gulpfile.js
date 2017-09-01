fb778a2aba4770b83de9fcb2c1754c8d190dd792
Barry Lawson <blawson@www.colorado.gov>
2017-09-01 16:04:43 -0600

    Updated Paragraph accordions to match ckeditor accordions

--- docroot/themes/ci_xy/gulpfile.js
+++ docroot/themes/ci_xy/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//var watch = require('gulp-watch');
+var watch = require('gulp-watch');
 var shell = require('gulp-shell');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync');
