c89357b9e3cef7b25ca0cbf41d7c7aa742da730e
Alfredo Nevarez <anevarez@Alfredos-MacBook-Pro.local>
2017-09-08 14:13:08 -0600

    Removing if statement that detects acquia settings.

--- docroot/sites/ci.colorado.gov/settings.php
+++ docroot/sites/ci.colorado.gov/settings.php
@@ -758,8 +758,8 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 // database in each site environment (Dev, Stage, or Prod). To use this
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-    $conf['acquia_hosting_settings_autoconnect'] = FALSE;
+
+  $conf['acquia_hosting_settings_autoconnect'] = FALSE;
   require '/var/www/site-php/coloradod8m/ci-settings.inc';
 
 
@@ -776,8 +776,7 @@ $databases['default']['default']['prefix'] = array(
     'sessions' => $shared,
     'users' => $shared,
     'users_roles' => $shared,
-  );
-  }
+
 
 
 $config_directories['sync'] = '../config/sync';
