d6d7192b38eea73ce0fcd3f3f86975d8c432bd75
anevarez <alfredo.nevarez@www.colorado.gov>
2017-10-24 13:38:02 -0600

    adding drush aliases to test so drush script can do more of the heavy lifting per install.

---
+++ docroot/sites/default/drushrc.php
@@ -0,0 +1,184 @@
+<?php
+
+
+$aliases['ag.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'ag.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+
+$aliases['cdle.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'cdle.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['cdphe.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'cdphe.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['cdps.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'cdps.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['ci.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'ci.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['dola.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'dola.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['dora.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'dora.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['dpa.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'dpa.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['estes.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'estes.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['hcpf.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'hcpf.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['revenue.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'revenue.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['sipa.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'sipa.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+$aliases['slb.dev'] = array(
+    'root' => '/var/www/html/coloradod8m.dev/docroot',
+    'ac-site' => 'coloradod8m',
+    'ac-env' => 'dev',
+    'ac-realm' => 'prod',
+    'uri' => 'slb.colorado.gov',
+    'remote-host' => 'coloradod8mdev.ssh.prod.acquia-sites.com',
+    'remote-user' => 'coloradod8m.dev',
+    'path-aliases' => array(
+        '%drush-script' => 'drush' . $drush_major_version,
+    )
+);
+
+
+$aliases['newCombinedGroup'] = array(
+    'site-list' => array(
+        // Dev group 1 sites
+        'ag.colorado.gov',
+        'cdle.colorado.gov',
+        // 7_36 group 2 sites
+        'cdphe.colorado.gov',
+        'dora.colorado.gov',
+    ),
+);
