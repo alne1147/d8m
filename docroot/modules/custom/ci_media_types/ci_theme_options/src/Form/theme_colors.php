617d27bcef6911425bb1018e29c7d3a1d18496f7
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-01-05 14:47:31 -0700

    removing modules it duped.

--- docroot/modules/custom/ci_media_types/ci_theme_options/src/Form/theme_colors.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-# Theme Color Options
-$form['type_options'] = array(
-    '#type' => 'value',
-    '#value' => array('color1' => t('Color 1'),
-        'color2' => t('Color 2'),
-        'color3' => t('Color 3'))
-);
