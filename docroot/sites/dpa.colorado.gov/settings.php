ba3e8b9282d3c50c9d543c221ceb69056029154f
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-03-05 13:17:52 -0700

    search connection override.

--- docroot/sites/dpa.colorado.gov/settings.php
+++ docroot/sites/dpa.colorado.gov/settings.php
@@ -775,11 +775,10 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
 }
 // </DDSETTINGS>
 
-
 $config['acquia_search.settings']['connection_override'] = [
     'scheme' => 'https',
     'port' => 443,
-    'host' => 'us-east-1-c1.acquia-search.com',
+    'host' => 'useast1-c1.acquia-search.com',
     'index_id' => 'CDIY-145117.dev.default',
     'derived_key' => '8e4e9b45b93ad5e8ddb0f3f68e680a5b8830bad2',
 ];
