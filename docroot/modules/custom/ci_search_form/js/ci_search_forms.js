0fc42302a2af41171065284a25c510e76b320ada
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-03-21 20:12:37 -0600

    enabled datepicker library and added them into start and end date fields in exposed search filter.

--- docroot/modules/custom/ci_search_form/js/ci_search_forms.js
+++ docroot/modules/custom/ci_search_form/js/ci_search_forms.js
@@ -7,8 +7,24 @@
                 $(".advancedSearchTog").click(function(){
                     $("#advancedSearch").toggle("slow");
                 });
+
             });
+
+        }
+    };
+
+    Drupal.behaviors.SearchCalendar = {
+        attach: function(context, settings) {
+            $(document).ready(function(){
+
+                $("#edit-created").datepicker();
+                $("#edit-created-lt").datepicker();
+
+
+            });
+
         }
     };
 
 }(jQuery));
+
