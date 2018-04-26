488adf6c910ae656f3f1d2a478da37aa894a21f2
Amanda Marshall <amanda.marshall@egov.com>
2018-04-26 11:09:19 -0600

    Removed memcache script from ci colorado settings for testing.

--- docroot/sites/ci.colorado.gov/settings.php
+++ docroot/sites/ci.colorado.gov/settings.php
@@ -787,11 +787,3 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
 }
 // </DDSETTINGS>
 
-if (isset($settings['memcache']['servers'])) {
-    // Memcache settings
-    $settings['cache']['bins']['bootstrap'] = 'cache.backend.memcache';
-    $settings['cache']['bins']['discovery'] = 'cache.backend.memcache';
-    $settings['cache']['bins']['config'] = 'cache.backend.memcache';
-    // Use memcache as the default bin
-    $settings['cache']['default'] = 'cache.backend.memcache';
-}
