cb43ef63cbb6f2f622938867f565d67f42c4d3d7
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-15 14:47:18 -0600

    testing sync directory removal to fix update error.

--- docroot/sites/ci.colorado.gov/settings.php
+++ docroot/sites/ci.colorado.gov/settings.php
@@ -774,7 +774,6 @@ if (file_exists('/var/www/site-php')) {
 //);
 
 
-$config_directories['sync'] = '../config/sync';
 $settings['install_profile'] = 'ci_start';
 $config['content_directory'] = '../content';
 $conf['file_temporary_path'] = 'tmp';
