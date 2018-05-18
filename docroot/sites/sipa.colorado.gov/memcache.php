6149d9bc509eed7060a4b21dbf522dfbf2217618
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-18 10:54:31 -0600

    adding memcache in seperate include when module is enabled.

---
+++ docroot/sites/sipa.colorado.gov/memcache.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($settings['memcache']['servers'])) {
+    // Memcache settings
+    $settings['cache']['bins']['bootstrap'] = 'cache.backend.memcache';
+    $settings['cache']['bins']['discovery'] = 'cache.backend.memcache';
+    $settings['cache']['bins']['config'] = 'cache.backend.memcache';
+    // Use memcache as the default bin
+    $settings['cache']['default'] = 'cache.backend.memcache';
+}
+$conf['acquia_purge_http'] = FALSE;
+$conf['acquia_purge_https'] = TRUE;
+
+ ?>
