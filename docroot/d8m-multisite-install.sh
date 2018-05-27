3fc8a3c2721892a38c22e6a1db9e41dbde6cd17e
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-26 22:19:23 -0600

    removed custom module list. put yes on user account creations.

--- docroot/d8m-multisite-install.sh
+++ docroot/d8m-multisite-install.sh
@@ -14,12 +14,30 @@ drush @sites   config-set system.site mail "no-reply@www.colorado.gov" -y
 
 #// User additions and role assignments.
 
-drush @sites  ucrt ci-nevarez --mail="alfredo.nevarez@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role administrator ci-nevarez; drush @sites  ucrt ci-jwathen --mail="jwathen@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role administrator ci-jwathen; drush @sites  ucrt ci-blawson --mail="blawson@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role administrator ci-blawson; drush @sites  ucrt ci-kharrison --mail="kristina.harrison@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role support_administrator ci-kharrison;  drush @sites  ucrt ci-twhatley --mail="travis.whatley@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role support_administrator ci-twhatley; drush @sites  ucrt ci-supportadmin --mail="supportadmin@example.com" --password="9xzfbddmus";  drush @sites  user-add-role support_administrator ci-supportadmin; drush @sites  ucrt ci-contentadmin --mail="contentadmin@example.com" --password="9xzfbddmus";  drush @sites  user-add-role content_administrator ci-contentadmin; drush @sites  ucrt ci-useradmin --mail="userad@example.com" --password="9xzfbddmus";  drush @sites  user-add-role user_administrator ci-useradmin; drush @sites  ucrt ci-structureadmin --mail="structure@example.com" --password="9xzfbddmus";  drush @sites  user-add-role structure_administrator ci-structureadmin; drush @sites  ucrt ci-amarshall --mail="amanda.marshall@www.colorado.gov" --password="9xzfbddmus";  drush @sites  user-add-role administrator ci-amarshall;
+drush @sites ucrt ci-nevarez --mail="alfredo.nevarez@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role administrator ci-nevarez --y; 
+drush @sites ucrt ci-jwathen --mail="jwathen@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role administrator ci-jwathen --y; 
+drush @sites ucrt ci-blawson --mail="blawson@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role administrator ci-blawson --y; 
+drush @sites ucrt ci-kharrison --mail="kristina.harrison@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role support_administrator ci-kharrison --y;  
+drush @sites ucrt ci-twhatley --mail="travis.whatley@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role support_administrator ci-twhatley --y; 
+drush @sites ucrt ci-supportadmin --mail="supportadmin@example.com" --password="9xzfbddmus" --y;  
+drush @sites user-add-role support_administrator ci-supportadmin --y; 
+drush @sites ucrt ci-contentadmin --mail="contentadmin@example.com" --password="9xzfbddmus" --y;  
+drush @sites user-add-role content_administrator ci-contentadmin --y; 
+drush @sites ucrt ci-useradmin --mail="userad@example.com" --password="9xzfbddmus" --y;  
+drush @sites user-add-role user_administrator ci-useradmin --y; 
+drush @sites ucrt ci-structureadmin --mail="structure@example.com" --password="9xzfbddmus" --y;  
+drush @sites user-add-role structure_administrator ci-structureadmin --y; 
+drush @sites ucrt ci-amarshall --mail="amanda.marshall@www.colorado.gov" --password="9xzfbddmus" --y;  
+drush @sites user-add-role administrator ci-amarshall --y;
 
 
 drush @sites  user-unblock --name= ci-jwathen,ci-structureadmin,ci-useradmin,ci-contentadmin,ci-supportadmin,ci-twhatley,ci-kharrison,ci-blawson,ci-nevarez,ci-amarshall;
 
-drush @sites  en -y $(cat custom_modules_list.txt)
 drush @sites  en ci_article_setup --y;
 drush @sites en ci_admin_tweaks --y;
 rdush @sites en ci_theme_options --y;
