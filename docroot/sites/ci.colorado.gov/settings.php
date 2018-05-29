f0abcbbde8113ef454cfe8ef3062f6a826b32455
alfredo nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-29 20:17:01 +0000

    Revert "D8P-268 add scheduler module (pull request #72)"
    This reverts pull request #72.
    
    > * Added scheduler module.
    > 
    > * Added scheduler module to install profile.

--- docroot/sites/ci.colorado.gov/settings.php
+++ docroot/sites/ci.colorado.gov/settings.php
@@ -774,7 +774,7 @@ if (file_exists('/var/www/site-php')) {
 //);
 
 
-$config_directories['sync'] = '../config/local-sync';
+$config_directories['sync'] = '../config/sync';
 $settings['install_profile'] = 'ci_start';
 $config['content_directory'] = '../content';
 $conf['file_temporary_path'] = 'tmp';
