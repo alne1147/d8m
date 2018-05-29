668d004e8782b9b6c7c22fb3d27d5ff62a32f41d
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-29 15:25:35 -0600

    staging and sync now added. Staging is for chunks. Sync is the big sync.

--- docroot/sites/ci.colorado.gov/settings.php
+++ docroot/sites/ci.colorado.gov/settings.php
@@ -775,6 +775,7 @@ if (file_exists('/var/www/site-php')) {
 
 
 $config_directories['sync'] = '../config/sync';
+$config_directories['staging'] = '../config/staging';
 $settings['install_profile'] = 'ci_start';
 $config['content_directory'] = '../content';
 $conf['file_temporary_path'] = 'tmp';
