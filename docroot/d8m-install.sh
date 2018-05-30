ded3f23e962f4291f8c436acdf922a5dbf836225
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-05-30 13:57:18 -0600

    auto import content.

--- docroot/d8m-install.sh
+++ docroot/d8m-install.sh
@@ -30,7 +30,7 @@ drush -y config-set system.performance css.preprocess 0;
 drush -y config-set system.performance js.preprocess 0;
 drush cim --partial sync --y;
 drush cr --y;
-drush dcdi;
+drush dcdi --y;
 
 
 echo "That's the end of the D8M install script!"
