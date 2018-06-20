9ea8f27dd0bc171c82e0f0c06b09df366ed15bb0
Alfredo Nevarez <jr.nevarez@gmail.com>
2018-06-20 11:10:07 -0600

    added focus to input after search icon is clicked. Removed border from input focus.

--- docroot/modules/custom/ci_search_form/js/searchToggle.js
+++ docroot/modules/custom/ci_search_form/js/searchToggle.js
@@ -2,19 +2,26 @@
     'use strict';
     Drupal.behaviors.SearchAdv = {
         attach: function(context, settings) {
+
             $(".search").attr("aria-hidden", true);
             // Toggle Expansion
             $(".header__search").off('click').click(function(){
+
                 $(".search").toggleClass("search--open");
                 $(".search").slideToggle(300);
+                $( "input#edit-search.search__form--input.input-lg").focus();
                 if ($(".search").hasClass("search--open")) {
                     $(".header__search--icon").addClass("search--open");
+
                     $(".search").attr("aria-hidden", false);
                 } else {
                     $(".header__search--icon").removeClass("search--open");
                     $(".search").attr("aria-hidden", true);
                 }
             });
+
+
+
             // Toggle Expansion with keyboard
             $(".header__search").keydown( function (e) {
                 // Enter and Space keys
