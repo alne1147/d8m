f56a715ec0a2675ff7c6e888b9b8a5492cbf7e25
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-06-22 10:18:24 -0600

    added border to last row of article views.

--- docroot/modules/custom/ci_views_tweaks/js/ci_reference_recent.js
+++ docroot/modules/custom/ci_views_tweaks/js/ci_reference_recent.js
@@ -12,6 +12,8 @@
             $( '.more-link' ).addClass( "col-sm-2" );
             $( '#views-exposed-form-references-recent-nodes-ref-nodes' ).addClass( "col-sm-8 offset-md-2" );
             $( '.view-article-blocks .view-footer' ).addClass( "col-sm-8 offset-md-2" );
+            $( '.view-article-blocks .views-row:last' ).css({ borderBottom: "1px solid",borderColor: "#DDDDDD", paddingBottom: "5px" });
+            $( '.view-references-recent-nodes .views-row:last' ).css({ borderBottom: "1px solid",borderColor: "#DDDDDD", paddingBottom: "5px" });
 
         }
     };
