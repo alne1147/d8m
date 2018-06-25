4791fb922254583f6af8dc76dca61e216d4cadd4
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-06-25 13:37:51 -0600

    removed line that was hiding ul's in sidebar menu ul li's.

--- docroot/modules/custom/ci_js/js/superfish_insert.js
+++ docroot/modules/custom/ci_js/js/superfish_insert.js
@@ -4,10 +4,11 @@
     Drupal.behaviors.sf2haduken = {
         attach: function(context, settings) {
 
+            $('.region-sidebar-second ul').addClass( "sf-menu" );
             $('.region-sidebar-second ul.menu').addClass( "menu sf-menu sf-main sf-vertical sf-style-none sf-js-enabled" );
             $('.region-sidebar-first ul.menu').addClass( "menu sf-menu sf-main sf-vertical sf-style-none sf-js-enabled" );
             $('.region-sidebar-first ul.menu a').removeClass("dropdown-toggle") .addClass( "menu sf-menu sf-main sf-vertical sf-style-none sf-js-enabled" );
-            $('.region-sidebar-first ul.menu ul').removeClass("dropdown-menu") .addClass( "sf-hidden" );
+            $("#edit_field_blocks_chosen option[value='blockmainfooter']").remove();
         }
     };
 
