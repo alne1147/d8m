6e5e793a2147547f243927f947926d36948cde87
Alfredo Nevarez <jr.nevarez@gmail.com>
2018-07-11 17:02:53 -0600

    added all others option. Prevent from duplicating as well.

--- docroot/modules/custom/ci_js/js/form_manipulations.js
+++ docroot/modules/custom/ci_js/js/form_manipulations.js
@@ -6,6 +6,10 @@
     Drupal.behaviors.menuFieldRestrictions = {
         attach: function(context, settings) {
 
+
+
+
+            $('.search-page--refine-wrapper select#edit-type').append('<option value="All">All Others</option>');
             $(".form-select option[value='account']").remove();
             $(".form-select option[value='admin']").remove();
             $(".form-select option[value='custom-menu']").remove();
@@ -13,6 +17,15 @@
             $(".form-select option[value='tools']").remove();
             $('select#edit-field-menu-reference-left').find('option[value=\'menu-10\']').appendTo('select#edit-field-menu-reference-left');
             $('select#edit-field-menu-reference-right').find('option[value=\'menu-10\']').appendTo('select#edit-field-menu-reference-right');
+
+            var usedNames = {};
+            $("select[name='type'] > option").each(function () {
+                if(usedNames[this.text]) {
+                    $(this).remove();
+                } else {
+                    usedNames[this.text] = this.value;
+                }
+            });
 
         }
     };
