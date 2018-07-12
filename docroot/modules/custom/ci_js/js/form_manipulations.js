7bc3c5096c087d88ca65ae7f03d9183b605849f0
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-07-12 11:23:06 -0600

    Removed unused options in search form under types. Adjusted column size of search fields type and tags.

--- docroot/modules/custom/ci_js/js/form_manipulations.js
+++ docroot/modules/custom/ci_js/js/form_manipulations.js
@@ -9,7 +9,7 @@
 
 
 
-            $('.search-page--refine-wrapper select#edit-type').append('<option value="All">All Others</option>');
+
             $(".form-select option[value='account']").remove();
             $(".form-select option[value='admin']").remove();
             $(".form-select option[value='custom-menu']").remove();
@@ -18,7 +18,16 @@
             $('select#edit-field-menu-reference-left').find('option[value=\'menu-10\']').appendTo('select#edit-field-menu-reference-left');
             $('select#edit-field-menu-reference-right').find('option[value=\'menu-10\']').appendTo('select#edit-field-menu-reference-right');
 
-            var usedNames = {};
+
+// Search form manipulations
+          $('.search-page--refine-wrapper select#edit-type').append('<option value="All">All Others</option>');
+          $(".search-page__list--type .form-select option[value='contact']").remove();
+          $(".search-page__list--type .form-select option[value='landing_page']").remove();
+          $(".search-page__list--type .form-select option[value='webform']").remove();
+          $(".search-page__list--type .form-select option[value='page']").remove();
+          $('select option:contains("Article")').text('Articles');
+          $('select option:contains("Event")').text('Events');
+          var usedNames = {};
             $("select[name='type'] > option").each(function () {
                 if(usedNames[this.text]) {
                     $(this).remove();
