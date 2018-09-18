12f5ecad534d22d993b5e07ad2bec483ea7ac4f7
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-09-18 10:09:15 -0600

    file entity not ready for config installation. Errors out.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1189,3 +1189,4 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
 // </DDSETTINGS>
 
 $settings['memcache']['stampede_protection'] = TRUE;
+$config['image.settings']['allow_insecure_derivatives'] = TRUE;
