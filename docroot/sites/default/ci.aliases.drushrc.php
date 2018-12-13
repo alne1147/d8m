14a97c4b3ea368188db81967e7b06e096c56897e
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-12-13 16:18:35 -0700

    updating.

---
+++ docroot/sites/default/ci.aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+$root = '/var/www/html/coloradod8mdev/docroot/';
+
+$aliases['ag'] = array(
+  'root' => $root,
+  'uri' => 'ag.colorado.gov',
+);
+
+$aliases['ci'] = array(
+  'root' => $root,
+  'uri' => 'ci.colorado.gov',
+);
