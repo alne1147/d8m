776d3e639a1811de65150fbc9361fe73d96ea998
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2018-12-21 12:03:40 -0700

    updating scripts for import and install.

--- ci_migrate_reset.sh
+++ ci_migrate_reset.sh
@@ -4,6 +4,7 @@ rm /Users/alfredonevarez/Sites/coloradod8m/config/synchronize/sync/migrate_plus.
 drush cim sync -y;
 drush en ci_migrate -y;
 drush cex sync -y;
-
-
+drush mim ci_user;
+drush mim ci_nodes;
+drush sql-sync  @d8m.local @estes.dev
 echo "CI Migrate reset."
--- config-delete.sh
+++ config-delete.sh
@@ -1,10 +1,9 @@
-rm -rf config/synchronize/sync/workflows.workflow.editorial.yml
-rm -rf config/synchronize/sync/views.view.moderated_content.yml
-rm -rf config/synchronizrm -rf config/synchronize/sync/workflows.workflow.editorial.yml
-rm -rf config/synchronize/sync/views.view.moderated_content.yml
-rm -rf config/synchronize/sync/system.site.yml
+git checkout ../config/synchronize/sync/acquia_connector.settings.yml
+rm -rf ../config/synchronize/sync/workflows.workflow.editorial.yml
+rm -rf ../config/synchronize/sync/views.view.moderated_content.yml
+rm -rf ../config/synchronize/sync/workflows.workflow.editorial.yml
+rm -rf ../config/synchronize/sync/views.view.moderated_content.yml
+rm -rf ../config/synchronize/sync/system.site.yml
 rm -rf config/synchronize/sync/acquia_connector.settings.yml
-
-echo "Removed unwanted config! Yes that fast."e/sync/acquia_connector.settings.yml
 
 echo "Removed unwanted config! Yes that fast."
