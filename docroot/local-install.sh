abcf4167128d91c1013386302f54b03caf25d645
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-01-03 08:51:59 -0700

    cleaning up install code.

--- docroot/local-install.sh
+++ docroot/local-install.sh
@@ -34,14 +34,4 @@ drush cim --partial post-sync -y;
 drush cr -y;
 drush uli;
 
-drush pm-uninstall ci_migrate -y;
-drush cex sync -y;
-rm /Users/alfredonevarez/Sites/coloradod8m/config/synchronize/sync/migrate_plus.migration.ci_*;
-drush cim sync -y;
-drush en ci_migrate -y;
-drush cex sync -y;
-drush mim ci_user;
-drush mim ci_nodes;
-drush sql-sync  @d8m.local @ceo.dev
-
 echo "That's the end of the D8M install script!"
