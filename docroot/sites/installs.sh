ebcb58773b538c6781af845c1a569be36f08da39
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-01-31 07:29:00 -0700

    adding better multisite install script in attempt to install sites 1 at a time and only on success.

---
+++ docroot/sites/installs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sh ag.colorado.gov/install.sh &&
+sh cdhs.colorado.gov/install.sh &&
+sh cdle.colorado.gov/install.sh &&
+sh cdphe.colorado.gov/install.sh &&
+sh cdps.colorado.gov/install.sh &&
+sh cdss.colorado.gov/install.sh &&
+sh ceo.colorado.gov/install.sh &&
+sh ci.colorado.gov/install.sh &&
+sh dhsem.colorado.gov/install.sh &&
+sh dola.colorado.gov/install.sh &&
+sh dora.colorado.gov/install.sh &&
+sh dpa.colorado.gov/install.sh &&
+sh estes.colorado.gov/install.sh &&
+sh sipa.colorado.gov/install.sh &&
+sh slb.colorado.gov/install.sh &&
+sh slb.colorado.gov/install.sh &&
