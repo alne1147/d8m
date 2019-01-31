dbabbd39f4b98fdba7e1c523275ebd52195f2184
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-01-31 07:51:56 -0700

    adding return directory variable.

--- docroot/sites/sites-install.sh
+++ docroot/sites/sites-install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-sh ag.colorado.gov/install.sh &&
-sh cdhs.colorado.gov/install.sh &&
-sh cdle.colorado.gov/install.sh &&
-sh cdphe.colorado.gov/install.sh &&
-sh cdps.colorado.gov/install.sh &&
-sh cdss.colorado.gov/install.sh &&
-sh ceo.colorado.gov/install.sh &&
-sh ci.colorado.gov/install.sh &&
-sh dhsem.colorado.gov/install.sh &&
-sh dola.colorado.gov/install.sh &&
-sh dora.colorado.gov/install.sh &&
-sh dpa.colorado.gov/install.sh &&
-sh estes.colorado.gov/install.sh &&
-sh sipa.colorado.gov/install.sh &&
-sh slb.colorado.gov/install.sh &&
-sh slb.colorado.gov/install.sh
+cd ag.colorado.gov/; sh install.sh; cd "$OLDPWD" &&
+cd cdhs.colorado.gov/;  sh install.sh; cd "$OLDPWD" &&
+cd cdle.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd cdphe.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd cdps.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd cdss.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd ceo.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd ci.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd dhsem.colorado.gov/; sh install.sh; cd "$OLDPWD" &&
+cd dola.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd dora.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd dpa.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd estes.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd sipa.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd slb.colorado.gov/; sh install.sh; cd "$OLDPWD"&&
+cd slb.colorado.gov/; sh install.sh; cd "$OLDPWD";
