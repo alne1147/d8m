54ee5d21849a2db62c8eb01591204248ca4a17ce
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-02-08 16:26:05 -0700

    adding domains back into sites file.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,11 +1,76 @@
 <?php
 
 $sites = [
-
-  'd8m' => 'ag',
+    
+  // 'nexus' => 'cdphe',
+	
     // Dept. of Agriculture
-    'ag.dev.colorado.gov' => 'ag',
-    'ag.stg.colorado.gov' => 'ag',
-    'ag.colorado.gov' => 'ag'
+    'ag.dev.colorado.gov' => 'ag.colorado.gov',
+    'ag.stg.colorado.gov' => 'ag.colorado.gov',
+    
+    // Dept. of Public Safety
+    'cdhs.dev.colorado.gov' => 'cdhs.colorado.gov',
+    'cdhs.stg.colorado.gov' => 'cdhs.colorado.gov',
+    
+    // Dept. of Public Safety
+    'cdps.dev.colorado.gov' => 'cdps.colorado.gov',
+    'cdps.stg.colorado.gov' => 'cdps.colorado.gov',
+    
+    
+    // Decision Support Systems
+    'cdss.dev.colorado.gov' => 'cdss.colorado.gov',
+    'cdss.stg.colorado.gov' => 'cdss.colorado.gov',
+    
+    // Energy Office
+    'ceo.dev.colorado.gov' => 'ceo.colorado.gov',
+    'ceo.stg.colorado.gov' => 'ceo.colorado.gov',
+    
+    // Dept. of Personnel and Administration
+    'dpa.dev.colorado.gov' => 'dpa.colorado.gov',
+    'dpa.stg.colorado.gov' => 'dpa.colorado.gov',
+    
+    // Dept. of Revenue
+    'revenue.dev.colorado.gov' => 'revenue.colorado.gov',
+    'revenue.stg.colorado.gov' => 'revenue.colorado.gov',
+    
+    // Dept. of Regulatory Agencies
+    'dora.dev.colorado.gov' => 'dora.colorado.gov',
+    'dora.stg.colorado.gov' => 'dora.colorado.gov',
+    
+    // Dept. of Local Affairs
+    'dola.dev.colorado.gov' => 'dola.colorado.gov',
+    'dola.stg.colorado.gov' => 'dola.colorado.gov',
+    
+    // Dept. of Healthcare Policy and Financing
+    'hcpf.dev.colorado.gov' => 'hcpf.colorado.gov',
+    'hcpf.stg.colorado.gov' => 'hcpf.colorado.gov',
+
+    // Department of Homeland Security & Emergency Management
+    'dhsem.dev.colorado.gov' => 'dhsem.colorado.gov',
+    'dhsem.stg.colorado.gov' => 'dhsem.colorado.gov',
+
+    // Dept. of Labor
+    'cdle.dev.colorado.gov' => 'cdle.colorado.gov',
+    'cdle.stg.colorado.gov' => 'cdle.colorado.gov',
+    
+    // Dept. of Public Health and Environment
+    'cdphe.dev.colorado.gov' => 'cdphe.colorado.gov',
+    'cdphe.stg.colorado.gov' => 'cdphe.colorado.gov',
+    
+    // Town of Estes Park
+    'estes.dev.colorado.gov' => 'estes.colorado.gov',
+    'estes.stg.colorado.gov' => 'estes.colorado.gov',
+    
+    // State Internet Portal Authority
+    'sipa.dev.colorado.gov' => 'sipa.colorado.gov',
+    'sipa.stg.colorado.gov' => 'sipa.colorado.gov',
+    
+    // State Land Board
+    'slb.dev.colorado.gov' => 'slb.colorado.gov',
+    'slb.stg.colorado.gov' => 'slb.colorado.gov',
+    
+    // Colorado Interactive
+    'ci.dev.colorado.gov' => 'ci.colorado.gov',
+    'ci.stg.colorado.gov' => 'ci.colorado.gov',
 
 ];
