cd3d4fe7ade379cd478d905d9b7639035a03e741
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-02-12 14:44:25 -0700

    adding roles script and first round site user addition scripts.

---
+++ scripts/roles.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+drush user-add-role administrator amarshall --y;
+drush user-add-role administrator rchung --y;
+drush user-add-role administrator aturner	--y;
+drush user-add-role administrator blawson --y;
+drush user-add-role administrator anevarez --y;
+drush user-add-role support_administrator supportadmin --y;
+drush user-add-role content_administrator contentadmin --y;
+drush user-add-role user_administrator useradmin --y;
+drush user-add-role structure_administrator structureadmin --y;
