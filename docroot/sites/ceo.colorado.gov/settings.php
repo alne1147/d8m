4773e87b9dd850168e50d19ed5e55525089aa579
Alfredo Nevarez <alfredo.nevarez@www.colorado.gov>
2019-02-20 16:07:38 -0700

    trying to fix ceo.

--- docroot/sites/ceo.colorado.gov/settings.php
+++ docroot/sites/ceo.colorado.gov/settings.php
@@ -768,7 +768,16 @@ $config_directories['sole'] = '../config/ceo.colorado.gov/sole';
 $settings['install_profile'] = 'ci_start';
 $config['content_directory'] = '../content';
 $conf['file_temporary_path'] = 'tmp';
-$config['system.site']['name'] = 'Department of Local Affairs';
+$config['system.site']['name'] = 'Statewide Internet Portal Authority';
+
+// Local or other non-acquia-hosted Drupal environment
+$config['acquia_search.settings']['connection_override'] = [
+  'scheme' => 'https',
+  'port' => 443,
+  'host' => 'useast1-c26.acquia-search.com',
+  'index_id' => 'CDIY-145117.dev.cicoloradogov',
+  'derived_key' => '4793e027a7f176fc49cf97c6bf7e68c6e53726cc',
+];
 
 // <DDSETTINGS>
 // Please don't edit anything between <DDSETTINGS> tags.
@@ -777,5 +786,83 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/cld_prod_coloradod8m_dev_ceo_colorado_gov.inc';
 }
 // </DDSETTINGS>
+
+
+// Check for PHP Memcached libraries.
+$memcache_exists = class_exists('Memcache', FALSE);
+$memcached_exists = class_exists('Memcached', FALSE);
+$memcache_module_is_present = file_exists(DRUPAL_ROOT . '/modules/contrib/
+memcache/memcache.services.yml');
+
+if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
+  // Use Memcached extension if available.
+  if ($memcached_exists) {
+    $settings['memcache']['extension'] = 'Memcached';
+  }
+  // Register required class namespaces.
+  if (class_exists(\Composer\Autoload\ClassLoader::class)) {
+    $class_loader = new \Composer\Autoload\ClassLoader();
+    $class_loader->addPsr4('Drupal\\memcache\\', 'modules/contrib/
+    memcache/src');
+    $class_loader->register();
+    $settings['container_yamls'][] = DRUPAL_ROOT . '/modules/contrib/memcache/
+    memcache.services.yml';
+    // Bootstrap cache.container with memcache rather than database.
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        'database' => [
+          'class' => 'Drupal\Core\Database\Connection',
+          'factory' => 'Drupal\Core\Database\Database::getConnection',
+          'arguments' => ['default'],
+        ],
+        'settings' => [
+          'class' => 'Drupal\Core\Site\Settings',
+          'factory' => 'Drupal\Core\Site\Settings::getInstance',
+        ],
+        'memcache.config' => [
+          'class' => 'Drupal\memcache\DrupalMemcacheConfig',
+          'arguments' => ['@settings'],
+        ],
+        'memcache.backend.cache.factory' => [
+          'class' => 'Drupal\memcache\DrupalMemcacheFactory',
+          'arguments' => ['@memcache.config'],
+        ],
+        'memcache.backend.cache.container' => [
+          'class' => 'Drupal\memcache\DrupalMemcacheFactory',
+          'factory' => ['@memcache.backend.cache.factory', 'get'],
+          'arguments' => ['container'],
+        ],
+        'lock.container' => [
+          'class' => 'Drupal\memcache\Lock\MemcacheLockBackend',
+          'arguments' => ['container', '@memcache.backend.cache.container'],
+        ],
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\Core\Cache\DatabaseCacheTagsChecksum',
+          'arguments' => ['@database'],
+        ],
+        'cache.container' => [
+          'class' => 'Drupal\memcache\MemcacheBackend',
+          'arguments' => [
+            'container',
+            '@memcache.backend.cache.container',
+            '@lock.container',
+            '@memcache.config',
+            '@cache_tags_provider.container',
+          ],
+        ],
+      ],
+    ];
+    // Override default fast chained backend for static bins.
+    // @see https://www.drupal.org/node/2754947
+    $settings['cache']['bins']['bootstrap'] = 'cache.backend.memcache';
+    $settings['cache']['bins']['discovery'] = 'cache.backend.memcache';
+    $settings['cache']['bins']['config'] = 'cache.backend.memcache';
+    // Use memcache as the default bin.
+    $settings['cache']['default'] = 'cache.backend.memcache';
+  }
+}
+$conf['acquia_purge_http'] = FALSE;
+$conf['acquia_purge_https'] = TRUE;
 
 $settings['memcache']['stampede_protection'] = TRUE;
